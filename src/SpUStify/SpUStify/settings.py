61c2ce6c39fa27387294dc70314e85e2bf924e31
Trinhhayoo <phuongtrinhh2003@gmail.com>
2023-09-02 14:11:48 +0700

    a

--- src/SpUStify/SpUStify/settings.py
+++ src/SpUStify/SpUStify/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'SPUSTIFY',
         'USER': 'postgres',
-        'PASSWORD': '11112003',
+        'PASSWORD': '123456789',
         'HOST': 'localhost',
         'PORT': '5432',
     }
