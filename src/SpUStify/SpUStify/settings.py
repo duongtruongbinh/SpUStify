ae7097be46c7d2f27aaebd3244b990246c7696ed
duongtruongbinh <duongtruongbinh2003@gmail.com>
2023-09-07 21:59:54 +0700

    Delete redundant files

--- src/SpUStify/SpUStify/settings.py
+++ src/SpUStify/SpUStify/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'SPUSTIFY',
         'USER': 'postgres',
-        'PASSWORD': '3107',
+        'PASSWORD': '11112003',
         'HOST': 'localhost',
         'PORT': '5432',
     }
